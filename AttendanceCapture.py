3e8925716b7e0cbaa38fd1c6a513b6193f0e8551
Sheetal Kumar <sheetal@impulsiveweb.com>
2023-09-05 01:20:38 +0530

    a

--- AttendanceCapture.py
+++ AttendanceCapture.py
@@ -28,6 +28,10 @@ class AttendanceCapture(object):
     options._debugger_address = '0.0.0.0:9222'
 
     chrome_options = webdriver.ChromeOptions()
+    prefs = {"download.default_directory": Path}
+    chrome_options.add_experimental_option("prefs", prefs)
+    chrome_options.add_argument("window-size=1920,1080")
+
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
